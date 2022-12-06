03d0d018a8f813c0ae1638150d6900f1780682c5
Mary <mary_hbb@yahoo.com>
2022-12-06 21:55:48 +0100

    add shopping cart item style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingCartItmeComponent } from './shopping-cart-itme/shopping-cart-itme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartItmeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shopping-cart-itme/shopping-cart-itme.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-cart-itme',
+  templateUrl: './shopping-cart-itme.component.html',
+  styleUrls: ['./shopping-cart-itme.component.scss']
+})
+export class ShoppingCartItmeComponent {
+
+}
