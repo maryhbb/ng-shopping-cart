917a7a470d70479b904ecf4268f372d194280fa5
Mary <mary_hbb@yahoo.com>
2022-12-21 16:48:59 +0100

    add refresh and count method and emiter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ShoppingCartItemModel} from "./shared/models/shopping-cart-item.model";
 
 @Component({
@@ -6,21 +6,34 @@ import {ShoppingCartItemModel} from "./shared/models/shopping-cart-item.model";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'shopping-cart';
-  items: ShoppingCartItemModel [] = [
-    {id: 1, count: 0, image: "assets/images/3_large.jpg", name: "iPhone 14 Pro", price: 1299},
-    {id: 2, count: 0, image: "assets/images/2_large.jpg", name: "iPhone 14", price: 999},
-    {id: 3, count: 0, image: "assets/images/1_large.jpg", name: "iPhone 13", price: 799},
-    {id: 4, count: 0, image: "assets/images/4_large.jpg", name: "iPhone SE", price: 549},
-    
-  ]
-  totalPrice :number = 0;
+  items: ShoppingCartItemModel [] = []
+  totalPrice: number = 0;
 
   onDeleteEvent($cartItemId: number) {
     const index = this.items.findIndex(item => item.id === $cartItemId);
     this.items.splice(index, 1);
   }
 
+  ngOnInit(): void {
+    this.initCart();
+  }
 
+  private initCart(){
+  this.items = [
+    {id: 1, count: 0, image: "assets/images/3_large.jpg", name: "iPhone 14 Pro", price: 1299},
+    {id: 2, count: 0, image: "assets/images/2_large.jpg", name: "iPhone 14", price: 999},
+    {id: 3, count: 0, image: "assets/images/1_large.jpg", name: "iPhone 13", price: 799},
+    {id: 4, count: 0, image: "assets/images/4_large.jpg", name: "iPhone SE", price: 549},
+  ]
+    this.refresh()
+  }
+  private refresh() {
+    let sumPrice: number = 0;
+    this.items.forEach(item=>{
+      sumPrice += item.price ?? 0;
+});
+    this.totalPrice = sumPrice
+  }
 }
--- src/app/shopping-cart-item/shopping-cart-item.component.ts
+++ src/app/shopping-cart-item/shopping-cart-item.component.ts
@@ -7,20 +7,28 @@ import {ShoppingCartItemModel} from "../shared/models/shopping-cart-item.model";
   styleUrls: ['./shopping-cart-item.component.scss']
 })
 export class ShoppingCartItemComponent {
-@Input() item :ShoppingCartItemModel = {};
-@Output() onDelete= new EventEmitter<number>();
+  @Input() item: ShoppingCartItemModel = {};
+  @Output() onDelete = new EventEmitter<number>();
+  @Output() onCountUpdated = new EventEmitter<ShoppingCartItemModel>;
+  @Output() onRefresh = new EventEmitter;
 
 
- add(){
-const currentCount :number = this.item.count ?? 0;
-this.item.count = currentCount +1;
+  add() {
+    const currentCount: number = this.item.count ?? 0;
+    this.item.count = currentCount + 1;
+    this.onCountUpdated.emit(this.item);
+    this.onRefresh.emit();
   }
- sub (){
-const currentCount :number = this.item.count ?? 0;
-this.item.count = currentCount > 0 ? (currentCount -1 ):0;
- }
 
- del(itemId?: number){
-this.onDelete.emit(itemId)
- }
+  sub() {
+    const currentCount: number = this.item.count ?? 0;
+    this.item.count = currentCount > 0 ? (currentCount - 1) : 0;
+    this.onCountUpdated.emit(this.item);
+    this.onRefresh.emit();
+  }
+
+  del(itemId?: number) {
+    this.onDelete.emit(itemId);
+    this.onRefresh.emit();
+  }
 }
