238ae1045706052015b08ae408355b96d6916beb
Mary <mary_hbb@yahoo.com>
2022-12-22 22:58:40 +0100

    last update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,14 +15,15 @@ export class AppComponent implements OnInit {
     const index = this.items.findIndex(item => item.id === $cartItemId);
     this.items.splice(index, 1);
   }
-  onCountUpdatedEvent($event: ShoppingCartItemModel){
+
+  onCountUpdatedEvent($event: ShoppingCartItemModel) {
     const index = this.items.findIndex(item => item.id === $event.id);
     this.items[index] = $event
   }
 
   refresh() {
     let sumPrice: number = 0;
-    this.items.forEach(item=>{
+    this.items.forEach(item => {
       sumPrice += (item.price ?? 0) * (item.count ?? 0);
     });
     this.totalPrice = sumPrice
@@ -32,14 +33,13 @@ export class AppComponent implements OnInit {
     this.initCart();
   }
 
-  private initCart(){
-  this.items = [
-    {id: 1, count: 0, image: "assets/images/3_large.jpg", name: "iPhone 14 Pro", price: 1299},
-    {id: 2, count: 0, image: "assets/images/2_large.jpg", name: "iPhone 14", price: 999},
-    {id: 3, count: 0, image: "assets/images/1_large.jpg", name: "iPhone 13", price: 799},
-    {id: 4, count: 0, image: "assets/images/4_large.jpg", name: "iPhone SE", price: 549},
-  ]
+  private initCart() {
+    this.items = [
+      {id: 1, count: 0, image: "assets/images/3_large.jpg", name: "iPhone 14 Pro", price: 1299},
+      {id: 2, count: 0, image: "assets/images/2_large.jpg", name: "iPhone 14", price: 999},
+      {id: 3, count: 0, image: "assets/images/1_large.jpg", name: "iPhone 13", price: 799},
+      {id: 4, count: 0, image: "assets/images/4_large.jpg", name: "iPhone SE", price: 549},
+    ]
     this.refresh()
   }
-
 }
